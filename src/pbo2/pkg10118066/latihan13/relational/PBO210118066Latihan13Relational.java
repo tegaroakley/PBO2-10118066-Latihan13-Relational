8719043c1061924fb2025903d611177b7f24c028
Tegar Lucky'Q Oakley <55655976+tegaroakley@users.noreply.github.com>
2019-09-24 16:01:04 +0900

    Add files via upload

---
+++ src/pbo2/pkg10118066/latihan13/relational/PBO210118066Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118066.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * NAMA  : Tegar Lucky'Q Oakley 
+ * NIM   : 10118066 
+ * KELAS : IF2 
+ * Deskripsi Program : Program ini berisi program untuk Menunjukkan relasi dari dua buah bilangan
+ */
+public class PBO210118066Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b  = " + (bil1 > bil2));
+        System.out.println("a < b  = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+        
+    }
+    
+}
